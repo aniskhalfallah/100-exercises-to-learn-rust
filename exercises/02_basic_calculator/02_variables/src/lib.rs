9f2f8c539c01d25f018c8bc4beb8919276570841
Anis KHALFALLAH <khalfallah.anis@hotmail.com>
2025-03-01 18:13:32 +0100

    fixed section 2 of exercices

--- exercises/02_basic_calculator/02_variables/src/lib.rs
+++ exercises/02_basic_calculator/02_variables/src/lib.rs
@@ -8,7 +8,8 @@
 pub fn speed(start: u32, end: u32, time_elapsed: u32) -> u32 {
     // TODO: define a variable named `distance` with the right value to get tests to pass
     //  Do you need to annotate the type of `distance`? Why or why not?
-
+    //  It is not needed to explicitly annotate the type of distance, it will be concluded by the compiler, as distance is end - start and both (end) and (start) are u32 type variables
+    let distance = end - start;
     // Don't change the line below
     distance / time_elapsed
 }
--- exercises/02_basic_calculator/06_while/src/lib.rs
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -4,7 +4,13 @@ pub fn factorial(n: u32) -> u32 {
     // interprets as "I'll get back to this later", thus
     // suppressing type errors.
     // It panics at runtime.
-    todo!()
+    let mut result: u32 = if n == 0 {1} else {n};
+    let mut i:u32 = n;
+    while i > 1 {
+        result = result * ( i-1 );
+        i = i-1;
+    }
+    return result;
 }
 
 #[cfg(test)]
